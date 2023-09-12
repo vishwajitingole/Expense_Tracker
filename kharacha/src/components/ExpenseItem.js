8110f3aac109402962d76a565f1818f3f5c8ac54
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-12 16:58:09 +0530

    Update ExpenseItem.js

--- kharacha/src/components/ExpenseItem.js
+++ kharacha/src/components/ExpenseItem.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './card';
+import {useState} from 'react';
 
-function ExpenseItem({ title, amount, date }) { // Destructure the props here
+
+function ExpenseItem({ title, amount, date })// Destructure the props here
+ {
+  //Use State is a react hook all the react hooks starts from keyword use
+  let [updatedtitle,setTitle]= useState(title);
+  //We need useState so that the changes will get reflected into the system
+  
+  let clickHandler=()=>{
+   setTitle("Hogya!");
+   
+  }
   return (
+    
     
     <Card className="expense-item">
       <ExpenseDate date={date} /> {/* Pass the date prop to ExpenseDate */}
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2 className='tittle'>{updatedtitle}</h2>
         <div className="expense-item__price">₹ {amount}</div>
-        <button onClick={()=>{alert("Clicked");}} >Ugly Button</button>
+        <button onClick={clickHandler} >Ugly Button</button>
       </div>
       </Card>
   );
